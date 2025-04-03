888ffdf418602eb5b941251a217b2767a7d69bd5
Esha Patel <Patellesha11@gmail.com>
2025-04-03 12:44:57 +0300

    added

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -5,6 +5,7 @@ function Character() {
   const [character, setCharacter] = useState("");
   return (
     <div className="cont">
+        <h1>LIVE CHARACTER COUNTER</h1>
       <textarea
         value={character}
         onChange={(e) => setCharacter(e.target.value)}
