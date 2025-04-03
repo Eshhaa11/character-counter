e5445db5acdd9953b684b62de3f8b86b38937552
Esha Patel <Patellesha11@gmail.com>
2025-04-03 11:39:26 +0300

    function declaration

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 function Character () {
+    const [character, setCharacter] = useState("");
     return (
         <div>
             <h1>LIVE CHARACTER COUNT</h1>
+            <textarea
+            value={}
         </div>
     )
 }
