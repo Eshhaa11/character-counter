5279ba4ffb39de9d88be766a435b7342619fb0d7
Esha Patel <Patellesha11@gmail.com>
2025-04-03 12:14:12 +0300

    added

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
-import './Character.css'
+import "./Character.css";
 
-function Character () {
-    const [character, setCharacter] = useState("");
-    return (
-        <div>
-           <textarea
-             value={character}
-            onChange={(e) => setCharacter(e.target.value)}
-            placeholder="Enter something"
-            rows="15"
-           />
-        </div>
-    )
+function Character() {
+  const [character, setCharacter] = useState("");
+  return (
+    <div>
+      <textarea
+        value={character}
+        onChange={(e) => setCharacter(e.target.value)}
+        placeholder="Enter something"
+        rows="15"
+      />
+      <p>Character Count: {character.length}</p>
+    </div>
+  );
 }
 
-export default Character
+export default Character;
